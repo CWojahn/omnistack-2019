fefe37f91f1fdf04b1a2664e3eea0b1dbc528511
Carlos Wojahn <carlos.wojahn@pce-eng.net>
2019-08-08 14:08:11 -0300

    Meio da terceira aula

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './login.css';
+
+import logo from '../assets/logo.svg';
+
+
+export default function Login(){
+    return(
+        <div className="login-container">
+            <form>
+                <img src={logo} alt ="Tindev"></img>
+                <input
+                   placeholder= "Digite seu usuário no Github"     
+                />
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    );
+}
+
