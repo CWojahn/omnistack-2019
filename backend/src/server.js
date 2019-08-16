fa4a37456d377917d9b5aa39b8e751810d7baf5a
Carlos W <carloswojahn@gmail.com>
2019-08-16 18:58:53 -0300

    feat: Aplicação completa.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://omnistack:senha@cluster0-5atvi.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+const connectedUsers ={};
+
+io.on('connection', socket =>{
+  const { user } = socket.handshake.query;
+  console.log(user, socket.id);
+  connectedUsers[user] = socket.id;
+  
+});
+
+
+mongoose.connect('mongodb+srv://omnistack:Desde_1989@cluster0-5atvi.mongodb.net/omnistack8?retryWrites=true&w=majority',{
   useNewUrlParser: true
 });
 
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req,res, next)=>{
+  
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
